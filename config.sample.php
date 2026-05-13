5a745607d54c8f4b70f80597ce3e9cf5fe6996a4
9163223-maker <9163223@gmail.com>
2026-05-13 20:35:56 +0300

    Document SMTP config fields

--- config.sample.php
+++ config.sample.php
@@ -1,8 +1,17 @@
 <?php
 return [
     'site_name' => 'PRAMIO',
-    'mail_to' => 'hello@pramio.ru',
+    'mail_to' => '9163223@gmail.com',
     'mail_from' => 'no-reply@pramio.ru',
+
+    // SMTP SpaceWeb / Sweb
+    'smtp_host' => 'smtp.spaceweb.ru',
+    'smtp_port' => 465,
+    'smtp_secure' => 'ssl',
+    'smtp_user' => 'no-reply@pramio.ru',
+    'smtp_password' => '',
+
+    // Optional Telegram duplication
     'tg_key' => '',
     'tg_chat' => '',
 ];
